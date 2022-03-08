b71b68d6dd60cb83a4d2944951886fb75973925d
BFSmx4 <97541925+BFSmx4@users.noreply.github.com>
2022-03-08 10:25:23 -0600

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -32,8 +32,8 @@ export default class RideScreen extends Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
+      /*status === "granted" es verdadero cuando el usuario concede el permiso.
+          status === "granted" es falso cuando el usuario no concede el permiso.
         */
       hasCameraPermissions: status === "granted",
       domState: "scanner",
@@ -65,11 +65,11 @@ export default class RideScreen extends Component {
   };
 
   assignBike = () => {
-    console.log("You have rented the bike for next 1 hour. Enjoy your ride!!");
+    console.log("Rentaste la bicicleta por 1 hora. ¡Disfruta tu viaje!");
   };
 
   returnBike = () => {
-    console.log("We hope you enjoyed your ride");
+    console.log("Esperamos que hayas disfrutado tu viaje.");
   };
 
   render() {
@@ -87,14 +87,14 @@ export default class RideScreen extends Component {
         <View style={styles.upperContainer}>
           <Image source={appIcon} style={styles.appIcon} />
           <Text style={styles.title}>e-ride</Text>
-          <Text style={styles.subtitle}>A Eco-Friendly Ride</Text>
+          <Text style={styles.subtitle}>Un viaje ecológico</Text>
         </View>
         <View style={styles.lowerContainer}>
           <View style={styles.textinputContainer}>
             <TextInput
               style={[styles.textinput, { width: "82%" }]}
               onChangeText={text => this.setState({ userId: text })}
-              placeholder={"User Id"}
+              placeholder={"Id del usuario"}
               placeholderTextColor={"#FFFFFF"}
               value={userId}
             />
@@ -102,7 +102,7 @@ export default class RideScreen extends Component {
           <View style={[styles.textinputContainer, { marginTop: 25 }]}>
             <TextInput
               style={styles.textinput}
-              placeholder={"Bicycle Id"}
+              placeholder={"Id de la bicicleta"}
               placeholderTextColor={"#FFFFFF"}
               value={bikeId}
             />
@@ -110,14 +110,14 @@ export default class RideScreen extends Component {
               style={styles.scanbutton}
               onPress={() => this.getCameraPermissions()}
             >
-              <Text style={styles.scanbuttonText}>Scan</Text>
+              <Text style={styles.scanbuttonText}>Escanear</Text>
             </TouchableOpacity>
           </View>
           <TouchableOpacity
             style={[styles.button, { marginTop: 25 }]}
             onPress={this.handleTransaction}
           >
-            <Text style={styles.buttonText}>Unlock</Text>
+            <Text style={styles.buttonText}>Desbloquear</Text>
           </TouchableOpacity>
         </View>
       </View>
